821f0a7975ef193f80ed2be9a0215dcf91eb4b14
Paulo Bezerra <pauloh2012sul@gmail.com>
2017-04-18 22:53:58 -0300

    PI LACOS

---
+++ EstruturasRepeticaoPI/src/SomaSeries.java
@@ -0,0 +1,24 @@
+
+public class SomaSeries {
+    
+    public static void main(String[] args) {
+        
+        int soma = 1;
+        int resultado = 0;
+        
+        System.out.println(soma);
+        
+        while(soma <10000){
+         
+         soma = soma  +2;
+         resultado = resultado+soma;
+         
+            System.out.println(soma);
+                
+        }System.out.println("O Resultado final é "+resultado);
+
+        
+        
+    }
+    
+}
